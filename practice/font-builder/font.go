e48762e9ae454f3c9d6c00b41f55b756cb4ed765
rolandelaigwu6-ui <rolandelaigwu6@gmail.com>
2026-05-24 00:24:57 +0100

    ascii-art freetime stud

--- practice/font-builder/font.go
+++ practice/font-builder/font.go
@@ -1,5 +1,29 @@
 package fontbuilder
 
 func BuildFont() map[byte][5]string {
-	return nil
+	return map[byte][5]string{
+
+		'1': {
+			"  |  ",
+			"  |  ",
+			"  |  ",
+			"  |  ",
+			"  |  ",
+		},
+		'0': {
+			" ___ ",
+			"|   |",
+			"|   |",
+			"|   |",
+			"|___|",
+		},
+		'2': {
+			" ___ ",
+			"    |",
+			" ___|",
+			"|    ",
+			"|___ ",
+		},
+	}
+
 }
--- practice/font-loader/loader.go
+++ practice/font-loader/loader.go
@@ -1,5 +1,34 @@
 package fontloader
 
 func LoadFont(lines []string, chars []byte) map[byte][5]string {
-	return nil
+	if len(lines) != len(chars)*5 {
+		return nil
+	}
+	// ch := map[byte][5]string {
+	// 	'1': {
+	// 		"  |  ",
+	// 		"  |  ",
+	// 		"  |  ",
+	// 		"  |  ",
+	// 		"  |  ",
+	// 	},
+	// 	'2': {
+	// 		" ___ ",
+	// 		"    |",
+	// 		" ___|",
+	// 		"|    ",
+	// 		"|___ ",
+	// 	},
+	// }
+
+	font_char := make(map[byte][5]string, len(chars))
+	for i, char := range chars {
+		startline := i * 5
+		var art [5]string
+		for row := 0; row < 5; row++ {
+			art[row] = lines[startline+row]
+		}
+		font_char[char] = art
+	}
+	return font_char
 }
